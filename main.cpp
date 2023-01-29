079ea8d8afd9c9aa9f79bf835e878843fd26b934
SuyeonSun <suninisy@gmail.com>
2023-01-29 17:33:14 +0900

    main branch push to origin/main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-# include <iostream>
-// using namepsace std;
-
-int main()
-{
-    int month = 8;
-    int date = 5;
-
-    std::cout << "오늘은 " << month << "월 " << date << "일 입니다." << std::endl;
-}
+//# include <iostream>
+//// using namepsace std;
+//
+//int main()
+//{
+//    int month = 8;
+//    int date = 5;
+//
+//    std::cout << "오늘은 " << month << "월 " << date << "일 입니다." << std::endl;
+//}
